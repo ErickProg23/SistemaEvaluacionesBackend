d53d6999b2f2b099fd67e32b2f0cdd5d61876f72
ErickProg23 <prog.erick.mtz@gmail.com>
2025-04-02 12:08:30 -0700

    Ultimos cambios para produccion

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import os
+
 class Config:
     # Configuración de la base de datos MySQL
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://develop:admin123@localhost/evaluaciones'
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'pruebas2025'
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
