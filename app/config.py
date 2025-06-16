cd5f6a0d1a7c8d89557655f7a07423aadaeaf919
ErickProg23 <prog.erick.mtz@gmail.com>
2025-06-16 11:00:54 -0700

    Cambios directos a produccion

--- app/config.py
+++ app/config.py
@@ -1,7 +1,15 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     # Configuración de la base de datos MySQL
+    print("DB_USER:", os.getenv('DB_USER'))
+    print("DB_PASSWORD:", os.getenv('DB_PASSWORD'))
+    print("DB_HOST:", os.getenv('DB_HOST'))
+    print("DB_NAME:", os.getenv('DB_NAME'))
+
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
