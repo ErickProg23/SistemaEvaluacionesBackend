6b69bf313862edd7dd927567f755b3df6289ee81
ErickProg23 <prog.erick.mtz@gmail.com>
2025-06-18 10:22:53 -0700

    revision de errores al guardar evaluaciones temporales

--- app/routes/evaluaciones_routes.py
+++ app/routes/evaluaciones_routes.py
@@ -1637,6 +1637,7 @@ def obtener_evaluaciones_empleado(empleado_id):
 @evaluacion_bp.route('/evaluaciones-temporales', methods=['OPTIONS', 'POST'])
 def guardar_evaluacion_temporal():
     data = request.get_json()
+    print("Datos recibidos", data)
 
     id_encargado = data.get('id_ecnargado')
     num_semana = data.get('num_semana')
